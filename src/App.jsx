ff55aa7dffc2b2db49f3429dde4045436f60c877
Serhii <sirozhka.v@gmail.com>
2024-04-11 13:21:36 +0200

    convert html to pfd function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
+import { useRef } from 'react';
 import './App.css';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 function App() {
+  const pdfRef = useRef();
+
+  const downloadPDF = () => {
+    const input = pdfRef.current;
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('p', 'mm', 'a4', true);
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = pdf.internal.pageSize.getHeight();
+      const imgWidth = canvas.width;
+      const imgHeight = canvas.height;
+      const ratio = Math.min(pdfWidth / imgWidth, pdfHeight / imgHeight);
+      const imgX = (pdfWidth - imgWidth * ratio) / 2;
+      const imgY = 30;
+      pdf.addImage(imgData, 'PNG', imgX, imgY, imgWidth * ratio, imgHeight * ratio);
+      pdf.save('invoice.pdf');
+    });
+  };
+
   return (
     <div className="container">
+      <button className="download-button" onClick={downloadPDF}>
+        Завантажити PDF
+      </button>
       <div className="invoice-wrapper">
-        <div className="invoice">
+        <div ref={pdfRef} className="invoice">
           <div className="invoice-content">
             <table className="clean">
               <tbody>
